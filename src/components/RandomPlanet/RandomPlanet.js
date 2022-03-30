6f7fbfce682c9278688fea0e4d30b1fe442666e3
dannotdead <daniil.koka2000@gmail.com>
2022-03-30 12:39:30 +0700

    feat: defaultProps

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -8,6 +8,10 @@ import ErrorIndicator from '../ErrorIndicator';
 
 class RandomPlanet extends Component {
 
+  static defaultProps = {
+    updateInterval: 10000
+  }
+
   swapiService = new SwapiService()
 
   state = {
@@ -16,8 +20,9 @@ class RandomPlanet extends Component {
   }
 
   componentDidMount() {
+    const { updateInterval } = this.props
     this.updatePlanet()
-    this.interval = setInterval(this.updatePlanet, 5000)
+    this.interval = setInterval(this.updatePlanet, updateInterval)
   }
 
   componentWillUnmount() {
